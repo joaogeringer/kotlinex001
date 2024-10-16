29ed22192fc35caa9f4f606deb838e4b2daff75e
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-10-16 15:34:37 -0300

    Create kotlin003.kt

---
+++ kotlin003.kt
@@ -0,0 +1,18 @@
+// função normal x função de uma linha
+fun main() {
+    //helloworld()
+    //println("A soma de 10 e 5 é ${sum(a = 10, b = 5)}")
+    println(divisao(10f, 3.5f))
+}
+
+fun sum (a: Int, b: Int): Int {
+    return(a+b)
+}
+
+fun sum2 (a: Int, b: Int) = (a + b)
+
+fun helloworld(){
+    println("Hello, world")
+}
+
+fun divisao(a: Float, b: Float) = a / b
