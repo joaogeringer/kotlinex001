7282fc07f28bc52911cc5d6b7890bec3f80e3681
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-10-16 16:09:06 -0300

    Create kotlin005.kt

---
+++ kotlin005.kt
@@ -0,0 +1,12 @@
+// kotlin math functions
+import kotlin.math.*
+
+fun main() {
+
+    println(max(5, 10))
+    println(min(5, 10))
+    println(sqrt(45.67f))
+    println(PI)
+    println(E)
+    println(round( x = 32132132.323232114))
+}
