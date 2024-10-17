0c01350d339a7d9609ce6da23314da351a1248d0
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-10-17 16:37:51 -0300

    Create kotlin007.kt

---
+++ kotlin007.kt
@@ -0,0 +1,15 @@
+fun caracteres(str: String): Int {
+    return str.length
+}
+
+fun cubo(n: Int): Int {
+    return n * n * n
+}
+
+fun milhasKm(milhas: Float): Float {
+    return milhas * 1.6f
+}
+
+fun main() {
+
+}
